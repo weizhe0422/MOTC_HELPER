7c117a711db89b6f35c174ba30ab24986ccfa21f
weizhe0422 <weizhe.chang@gmail.com>
2017-06-20 00:43:02 +0800

    no message

--- main.go
+++ main.go
@@ -13,14 +13,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -58,7 +56,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			case *linebot.TextMessage:
 				var station *THSRStation
 				//var timeTable []*StationtimeTable
-				//var timeTable *StationtimeTable
+				var timeTable *StationtimeTable
 				log.Println(message.Text)
 				inText := strings.ToLower(message.Text)
 
@@ -74,16 +72,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							out = fmt.Sprintf("您好，車站資訊：名稱%s, 編號為:%s, 地址: %s, 精度: %f, 緯度: %f", station.StationName.ZhTw, station.StationID, station.StationAddress, station.StationPosition.PositionLat, station.StationPosition.PositionLon)
 							stationID, _ := strconv.Atoi(station.StationID)
 							//timeTable = timeTableDB.GetFutTimetable(stationID)
-							//timeTable = timeTableDB.GetNextTimetabledata(stationID)
-
-							url := URLDailyTimetable + strconv.Itoa(stationID) + "/" + time.Now().Format("2006-01-02") + "?$top=30&$format=JSON"
-							c := NewClient(url)
-							body, _ := c.GetHttpRes()
-							var result []StationtimeTable
-							err = json.Unmarshal(body, &result)
-							out = out + fmt.Sprintf("Timetable長度:%d", len(result))
-
-							out = out + fmt.Sprintf("可搭班次: 車次代號:%s, 到達時間:%s, 終點站:%s", result[0].TrainNo, result[0].ArrivalTime, result[0].EndingStationName)
+							timeTable = timeTableDB.GetNextTimetabledata(stationID)
+							out = out + fmt.Sprintf("資料順序: %d", timeTableDB.getNextIndex())
+							out = out + fmt.Sprintf(" 可搭班次: 車次代號:%s, 到達時間:%s, 終點站:%s", timeTable.TrainNo, timeTable.ArrivalTime, timeTable.EndingStationName)
 						}
 					}
 					if out == "" {
