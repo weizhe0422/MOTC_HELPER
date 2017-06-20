a13585743b2a40cf3bcb8e72c314466bd3356e78
weizhe0422 <weizhe.chang@gmail.com>
2017-06-21 00:28:22 +0800

    no message

--- timetables.go
+++ timetables.go
@@ -63,12 +63,12 @@ func (t *Timetables) GetFutTimetable(stationNo int) []*StationtimeTable {
 	result := []*StationtimeTable{}
 	for index := 1; index <= len(t.allTimetable); index++ {
 		timetable := t.GetNextTimetabledata(stationNo)
-		//arriveTime, _ := time.Parse("2016-01-02 03-04", timetable.TrainDate+" "+timetable.ArrivalTime)
+		arriveTime, _ := time.Parse("2016-01-02 03-04", timetable.TrainDate+" "+timetable.ArrivalTime)
 
-		//hh, _ := time.ParseDuration("1h")
-		/*if arriveTime.After(time.Now().Add(-3*hh)) && arriveTime.After(time.Now().Add(13*hh)) {
+		hh, _ := time.ParseDuration("1h")
+		if arriveTime.After(time.Now().Add(-3*hh)) && arriveTime.After(time.Now().Add(1*hh)) {
 			result = append(result, timetable)
-		}*/
+		}
 		result = append(result, timetable)
 	}
 
